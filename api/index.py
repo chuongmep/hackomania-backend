b51e00ddf63ef4cecd577f585ba28a5b9d0361f9
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2024-04-06 15:25:35 +0800

    try addd sanic

--- api/index.py
+++ api/index.py
@@ -1,12 +1,9 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
-    
-    
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
+ 
+ 
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
